9283d74dd8e92722fbadfefd0956d6cf248bfaab
micah-h-o <micah.hayes.mail@gmail.com>
2026-08-11 16:35:21 -0400

    modify next.config for static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: 'micah.com',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
