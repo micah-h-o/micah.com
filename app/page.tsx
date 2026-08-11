e077413b0b44d3c71e484bb52e3ac1420e3dd29f
micah-h-o <micah.hayes.mail@gmail.com>
2026-08-11 16:32:54 -0400

    new initial version

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,60 @@
-import Image from "next/image";
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <article className="min-h-full flex flex-col bg-background font-sans text-sm gap-8 max-w-xs">
+      <header className='flex flex-col gap-1'>
+        <h1>Micah Hayes Omondi</h1>
+        <p className='text-font-secondary'><time dateTime="2026-08-10">Updated on August 10, 2026</time></p>
+      </header>
+
+      <section className='flex flex-col gap-4'>
+        <p>Hey, welcome to my corner of the Internet.</p>
+        <p>I'm a software engineer finishing up a computer science degree at the University of Waterloo. My favourite course was CS466, a graduate course in advanced algorithms.</p>
+        <p>After high school, I did Google CSSI. Then, I started building some production applications, in applied AI and fullstack engineering.</p>
+        <p>I built <Link href='https://usedipole.com'>Dipole</Link>, a tool that optimizes LLM citations and search rankings for companies.</p>
+        <p>I built <Link href='https://useharbory.com'>Harbory</Link>, an 
+        end-to-end workflow management platform for service companies.</p>
+        <p>I built <Link href='https://neulo.app'>Neulo</Link>, an AI-powered journal that surfaces insights and helps guide you
+        through journaling.</p>
+        <p>I'm currently looking for new opportunities.</p>
+      </section>
+
+        <ul className='flex flex-wrap gap-4'>
+          <li>
+            <Link 
+              href='https://github.com/micah-h-o' 
+              className='text-font-secondary'
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              GitHub
+            </Link>
+          </li>
+          <li>
+            <Link 
+              href='https://linkedin.com/in/micah-hayes-o' 
+              className='text-font-secondary'
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+              LinkedIn
+            </Link>
+          </li>
+          <li>
+            <Link 
+              href='https://x.com/micahomnd' 
+              className='text-font-secondary'
+            >
+              X (Twitter)
+            </Link>
+          </li>
+          <li>
+            <Link 
+              href='mailto:micah.hayes.mail@gmail.com'
+              className='text-font-secondary'
+            >
+              Email
+            </Link>
+          </li>
+        </ul>
+    </article>
   );
 }
+
