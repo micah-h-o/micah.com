6d18b87241b5386497733cc47f818ef96f15b7fe
micah-h-o <micah.hayes.mail@gmail.com>
2026-08-12 15:50:53 -0400

    use vercel instead, remove github pages setup and static export

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/micah.com',
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
