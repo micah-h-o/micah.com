6ce78f8aabbfcf6e29bea4ef9aec31a69e2371bf
micah-h-o <micah.hayes.mail@gmail.com>
2026-08-12 15:42:49 -0400

    prep. for github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: 'micah.com',
+  basePath: '/micah.com',
   images: {
     unoptimized: true,
   },
